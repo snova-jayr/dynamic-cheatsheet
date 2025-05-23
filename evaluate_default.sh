9c85592ac66a6f802102bf86ed0a2c7ec667692a
shubhangiu <shubhangi.upasani@sambanovasystems.com>
2025-05-23 13:29:53 -0700

    add gpt changes

--- evaluate_default.sh
+++ evaluate_default.sh
@@ -1,4 +1,4 @@
-SAMBANOVA_API_KEY=... python run_benchmark.py --task "CUSTOM_AIME_2024" --approach "default" \
+SAMBANOVA_API_KEY=9bee3459-3e28-47b9-b0e6-2e54b923ab49   python run_benchmark.py --task "CUSTOM_AIME_2024" --approach "default" \
     --model_name "sambanova/DeepSeek-V3-0324" \
     --save_directory "temp_output" \
     --generator_prompt_path "prompts/generator_prompt.txt" \
--- evaluate_dynamic_cummulative.sh
+++
@@ -1,6 +0,0 @@
-SAMBANOVA_API_KEY=... python run_benchmark.py --task "CUSTOM_AIME_2024" --approach "DynamicCheatsheet_Cumulative" \
-    --model_name "sambanova/DeepSeek-V3-0324" \
-    --save_directory "temp_output" \
-    --generator_prompt_path "prompts/generator_prompt.txt" \
-    --cheatsheet_prompt_path "prompts/curator_prompt_for_dc_cumulative.txt" \
-    --max_num_rounds 5
