52784caf80d4e4cf28507ab8275e49243255956c
shubhangiu <shubhangi.upasani@sambanovasystems.com>
2025-06-04 12:49:24 -0700

    add DC fixes

--- evaluate_dynamic_cummulative.sh
+++ evaluate_dynamic_cummulative.sh
@@ -1,6 +1,6 @@
-SAMBANOVA_API_KEY=... python run_benchmark.py --task "CUSTOM_AIME_2024" --approach "DynamicCheatsheet_Cumulative" \
-    --model_name "sambanova/DeepSeek-V3-0324" \
-    --save_directory "temp_output" \
+SAMBANOVA_API_KEY=9bee3459-3e28-47b9-b0e6-2e54b923ab49 python run_benchmark_finlora.py --dataset "financebench"  --approach "DynamicCheatsheet_Cumulative" \
+    --model_name "sambanova/Meta-Llama-3.1-8B-Instruct" \
+    --save_directory "finlora_derisking_dc"  \
     --generator_prompt_path "prompts/generator_prompt.txt" \
-    --cheatsheet_prompt_path "prompts/curator_prompt_for_dc_cumulative.txt" \
-    --max_num_rounds 5
+     --cheatsheet_prompt_path "prompts/curator_prompt_for_dc_cumulative.txt" \
+     --no_shuffle --max_tokens 1024 --max_num_rounds 2 
