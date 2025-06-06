a38b2e715a3d2450c192e62bf52c27410ed6d022
shubhangiu <shubhangi.upasani@sambanovasystems.com>
2025-06-05 21:07:35 -0700

    remove api keys

--- evaluate_default.sh
+++ evaluate_default.sh
@@ -1,4 +1,4 @@
-SAMBANOVA_API_KEY=9bee3459-3e28-47b9-b0e6-2e54b923ab49 python run_benchmark_finlora.py --dataset "financebench"  --approach "default" \
+SAMBANOVA_API_KEY=[...] python run_benchmark_finlora.py --dataset "financebench"  --approach "default" \
     --model_name "sambanova/Meta-Llama-3.1-8B-Instruct" \
     --save_directory "finlora_derisking"  \
     --generator_prompt_path "prompts/generator_prompt.txt" \
