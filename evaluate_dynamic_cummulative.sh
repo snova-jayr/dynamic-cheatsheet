a7bfd5a64bf96787cfa56f8351aa13b91a08aafe
shubhangiu <shubhangi.upasani@sambanovasystems.com>
2025-06-05 22:37:45 -0700

    add final answer extraction

--- evaluate_dynamic_cummulative.sh
+++ evaluate_dynamic_cummulative.sh
@@ -1,6 +1,6 @@
-SAMBANOVA_API_KEY=[...] python run_benchmark_finlora.py --dataset "financebench"  --approach "DynamicCheatsheet_Cumulative" \
+SAMBANOVA_API_KEY=9bee3459-3e28-47b9-b0e6-2e54b923ab49 python run_benchmark_finlora.py --dataset "financebench"  --approach "DynamicCheatsheet_Cumulative" \
     --model_name "sambanova/Meta-Llama-3.1-8B-Instruct" \
     --save_directory "finlora_derisking_dc"  \
     --generator_prompt_path "prompts/generator_prompt.txt" \
-     --cheatsheet_prompt_path "prompts/curator_prompt_for_dc_cumulative.txt" \
-     --no_shuffle --max_tokens 1024 --max_num_rounds 2 
+     --cheatsheet_prompt_path "prompts/curator_prompt_for_financebench.txt" \
+     --no_shuffle --max_tokens 2048 
