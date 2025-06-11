ae2ea43998a04954f9c3d9a73e36b3399f8e79a8
shubhangiu <shubhangi.upasani@sambanovasystems.com>
2025-06-10 17:36:03 -0700

    clean up code

--- generate_cheatsheet_sn.py
+++ generate_cheatsheet_sn.py
@@ -1,3 +1,4 @@
+import argparse 
 import json
 import ast 
 from utils_cheatsheet import *
@@ -5,44 +6,67 @@ import openai
 import time 
 import random 
 
+
+#### API key information ####
+
 api_key = "9bee3459-3e28-47b9-b0e6-2e54b923ab49"
 base_url = "https://api.sambanova.ai/v1"
 
-dataset_path = "/import/ml-sc-scratch2/shubhangiu/jays_dc_repo/dynamic-cheatsheet/data/finlora/train/financebench_train.jsonl"
+###---------------------####
 
-client = openai.OpenAI(api_key=api_key, base_url=base_url)
+def parse_args():
+    parser = argparse.ArgumentParser(description='Description of your program.')
+    parser.add_argument("--dataset_path", required=True, type=str)
+    parser.add_argument("--num_samples", default=-1, type=int)
+    parser.add_argument("--save_path", type=str, required=True)
+    parser.add_argument("--model_name", type=str, required=True)
+    args = parser.parse_args()
+    return args
 
-with open(dataset_path, 'r') as json_file:
-    all_samples = list(json_file)
 
-random.shuffle(all_samples)
-all_samples = all_samples[:50]
+def initialize_client():
+    client = openai.OpenAI(api_key=api_key, base_url=base_url)
+    return client 
 
-question_counts = 1
+def main():
+    args = parse_args()
+    client = initialize_client()
 
-old_cheatsheet = "(empty)"
+    with open(args.dataset_path, 'r') as json_file:
+        all_samples = list(json_file)
 
-for sample in all_samples:
-    time.sleep(60)
-    print(f"==========processing question {question_counts}==========")
-    task_dict = ast.literal_eval(sample)
-    all_context  = task_dict["context"]
-    question, context = all_context.split("\nDocument Pages Context")
-    gt_answer = task_dict["target"]
+    if args.num_samples != -1: 
+        random.shuffle(all_samples)
+        all_samples = all_samples[:args.num_samples]
+
+    question_counts = 1
+
+    old_cheatsheet = "(empty)"
+
+    for sample in all_samples:
+        time.sleep(60)
+        print(f"==========processing question {question_counts}==========")
+        task_dict = ast.literal_eval(sample)
+        all_context  = task_dict["context"]
+        question, context = all_context.split("\nDocument Pages Context")
+        gt_answer = task_dict["target"]
    
-    # generate cheatsheet
-    prompt = cheatsheet_gen_smaller_prompt.format(old_cheatsheet, question, context, gt_answer)
+        # generate cheatsheet
+        prompt = cheatsheet_gen_smaller_prompt.format(old_cheatsheet, question, context, gt_answer)
 
-    response = client.chat.completions.create(
-                    model="Llama-4-Maverick-17B-128E-Instruct",
+        response = client.chat.completions.create(
+                    model=args.model_name,
                     messages=[{"role": "user", "content": prompt}],
                     temperature=0.0
-                )
+        )
 
-    response = response.choices[0].message.content
-    new_cheatsheet = extract_cheatsheet(response, old_cheatsheet)
-    old_cheatsheet = new_cheatsheet
-    question_counts += 1
+        response = response.choices[0].message.content
+        new_cheatsheet = extract_cheatsheet(response, old_cheatsheet)
+        old_cheatsheet = new_cheatsheet
+        question_counts += 1
 
-# save generated cheatsheet
-open("generated_training_cheatsheets/financebench_cheatsheet_train_llama4_smaller_prompt.txt", "w+").write(new_cheatsheet)
+    # save generated cheatsheet
+    open(args.save_path, "w+").write(new_cheatsheet)
+
+if __name__ == "__main__":
+    main()
