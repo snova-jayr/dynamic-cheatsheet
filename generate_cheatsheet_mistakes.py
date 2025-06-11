8953fc6957ac16412ad35b2a1ee3ed7ab2b53c66
shubhangiu <shubhangi.upasani@sambanovasystems.com>
2025-06-10 17:40:07 -0700

    remove api key

--- generate_cheatsheet_mistakes.py
+++ generate_cheatsheet_mistakes.py
@@ -5,7 +5,7 @@ import openai
 import time 
 
 openai.api_type = "azure"
-openai.api_key = "983d9e08a78c4c2d8e89dcfac2de5605"
+openai.api_key = ""
 openai.api_base = "https://snova.openai.azure.com"
 openai.api_version = "2024-12-01-preview"
 openai.azure_endpoint="https://snova.openai.azure.com/"
