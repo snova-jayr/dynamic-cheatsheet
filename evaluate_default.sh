4870df6b4d583aa6fda45c337dc2cfacd64f34a3
shubhangiu <shubhangi.upasani@sambanovasystems.com>
2025-06-24 16:42:19 -0700

    add updates

--- evaluate_default.sh
+++ evaluate_default.sh
@@ -1,5 +1,5 @@
-SAMBANOVA_API_KEY=[...] python run_benchmark_finlora.py --dataset "financebench"  --approach "default" \
+SAMBANOVA_API_KEY=[...]  python run_benchmark_finlora.py --dataset "xbrl_finer"  --approach "default" \
     --model_name "sambanova/Meta-Llama-3.1-8B-Instruct" \
-    --save_directory "finlora_derisking"  \
+    --save_directory "finlora_derisking_xbrl_final"  \
     --generator_prompt_path "prompts/generator_prompt.txt" \
-     --no_shuffle --max_tokens 2048
+     --no_shuffle --max_tokens 32768  --sample_ratio 0.06
--- evaluate_dynamic_cummulative.sh
+++ evaluate_dynamic_cummulative.sh
@@ -1,6 +1,6 @@
-SAMBANOVA_API_KEY=[...] python run_benchmark_finlora.py --dataset "financebench"  --approach "DynamicCheatsheet_Cumulative" \
-    --model_name "sambanova/Meta-Llama-3.1-8B-Instruct" \
-    --save_directory "finlora_derisking_dc"  \
+SAMBANOVA_API_KEY=[...] python run_benchmark_finlora.py --dataset "xbrl_finer"  --approach "DynamicCheatsheet_Cumulative" \
+    --model_name "sambanova/Llama-4-Maverick-17B-128E-Instruct" \
+    --save_directory "finlora_derisking_xbrl_final"  \
     --generator_prompt_path "prompts/generator_prompt.txt" \
      --cheatsheet_prompt_path "prompts/curator_prompt_for_financebench.txt" \
-     --no_shuffle --max_tokens 8192 --max_num_rounds 1 
+     --no_shuffle --max_tokens 32768 --max_num_rounds 1 --sample_ratio 0.06
